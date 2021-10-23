a40305f8fe0b7bafabe7ab6e64712b7c74c8e66b
Albert Go <albertgo98@gmail.com>
2021-10-22 20:27:25 -0400

    Add files via upload

--- GeneticProgramDCSR2.cpp
+++ GeneticProgramDCSR2.cpp
@@ -63,7 +63,7 @@ int main(int argc, const char * argv[]) {
     vector<string> terminal{"val", "x", "x", "T"};
     
     vector<int> cross1{6, 13, 14, 27, 28, 29, 30, 55, 56, 57, 58, 59, 60, 61, 62, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254};
-    vector<int> cross2{3, 7, 8, 15, 16, 17, 18, 31, 32, 33, 34, 35, 36, 37, 38, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158};
+    vector<int> cross2{1, 3, 4, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20, 21, 22, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190};
     vector<int> cross3{5, 11, 12, 23, 24, 25, 26, 47, 48, 49, 50, 51, 52, 53, 54, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222};
     
     vector<Equation> population = get_population(coordinates, operations, terminal);
@@ -84,7 +84,7 @@ int main(int argc, const char * argv[]) {
                     }
                 }
             }
-            breed(coordinates, operations, terminal, population[i], population[parent2], population, cross2, cross3, new_population);
+            breed(coordinates, operations, terminal, population[i], population[parent2], population, cross2, cross2, new_population);
         }
         population = new_population;
 //        cout << population.size() << endl;
@@ -97,10 +97,10 @@ int main(int argc, const char * argv[]) {
         if (evaluations % 50 == 0){
             cout << "Status update: ";
             cout << population[0].error << endl;
-            cout << "Updating population..." << endl;
-            vector<Equation> new_set = get_population(coordinates, operations, terminal);
-            population.insert(population.end(), new_set.begin(), new_set.end());
-            sort(population.begin(), population.end(), compareByError);
+//            cout << "Updating population..." << endl;
+//            vector<Equation> new_set = get_population(coordinates, operations, terminal);
+//            population.insert(population.end(), new_set.begin(), new_set.end());
+//            sort(population.begin(), population.end(), compareByError);
             vector<string> curr_best = population[0].function;
             cout << "[";
             for(int j=0; j < curr_best.size(); j++){
@@ -187,24 +187,26 @@ vector<string> random_equation(vector<string> &operators, vector<string> &termin
     int half_size = (formula_size-1)/2;
     
     for (int i=0; i < formula_size; i++){
-        int rand_num = rand() % 10;
+        int rand_num = rand() % 15;
 
         if (i == 0){
-            if (rand_num < 9){
-                int rand_num2 = rand() % 6;
-                func.push_back(operators[rand_num2]);
-            }
-            else{
-                int rand_num2 = rand() % 3;
-                if (terminals[rand_num2] == "val"){
-                    int rand_num3 = rand() % 20;
-                    string s = to_string(rand_num3+1);
-                    func.push_back(s);
-                }
-                else{
-                    func.push_back(terminals[rand_num2]);
-                }
-            }
+            int rand_num2 = rand() % 6;
+            func.push_back(operators[rand_num2]);
+//            if (rand_num < 14){
+//                int rand_num2 = rand() % 6;
+//                func.push_back(operators[rand_num2]);
+//            }
+//            else{
+//                int rand_num2 = rand() % 3;
+//                if (terminals[rand_num2] == "val"){
+//                    int rand_num3 = rand() % 20;
+//                    string s = to_string(rand_num3+1);
+//                    func.push_back(s);
+//                }
+//                else{
+//                    func.push_back(terminals[rand_num2]);
+//                }
+//            }
         }
         else if ((i-1) >= 0 && (i-1) % 2 == 0){
             int parent = (i-1)/2;
@@ -221,7 +223,7 @@ vector<string> random_equation(vector<string> &operators, vector<string> &termin
                     }
                 }
                 else{
-                    if (rand_num < 9){
+                    if (rand_num < 14){
                         int rand_num2 = rand() % 6;
                         func.push_back(operators[rand_num2]);
                     }
@@ -261,7 +263,7 @@ vector<string> random_equation(vector<string> &operators, vector<string> &termin
                         }
                     }
                     else{
-                        if (rand_num < 9){
+                        if (rand_num < 14){
                             int rand_num2 = rand() % 6;
                             func.push_back(operators[rand_num2]);
                         }
